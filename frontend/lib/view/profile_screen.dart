91f1a263af11a32f052ca92747aa05e841bcc345
Sal Bhend <lbhend@uco.edu>
2026-02-17 00:43:40 -0600

    add images and profile screen work

--- frontend/lib/view/profile_screen.dart
+++ frontend/lib/view/profile_screen.dart
@@ -13,8 +13,98 @@ class ProfileScreen extends StatefulWidget {
 class ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Profile screen"),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              clipBehavior: Clip.none,
+              children: [
+                Container(
+                  //sky background for profile "bar"
+                  height: 180,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('images/skyProfileBG.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                Positioned(
+                  bottom: -90,
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.white, // border color
+                        width: 5, // border thickness
+                      ),
+                    ),
+                    child: ClipOval(
+                      //profile picture
+                      child: Image.asset(
+                        'images/nicobarPigeon.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 100),
+
+            const Text(
+              //Profile 'label' meaning the users username
+              "Username", //should be the person's username
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+
+            Padding(
+              //All text
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Username", //label
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.left,
+                  ),
+                  const SizedBox(height: 5), //spacer
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Username',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.edit),
+                      ),
+                      const SizedBox(width: 5),
+                    ],
+                  ),
+
+                  const Text(
+                    "Password",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
