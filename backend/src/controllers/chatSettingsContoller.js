cb8cdeeec899ca3f2cfb1d01f4ca5545d679678e
MoniqueAgura <magura@uco.edu>
2026-03-27 22:57:51 -0500

    update chatSettingsController.js

--- backend/src/controllers/chatSettingsContoller.js
+++ backend/src/controllers/chatSettingsContoller.js
@@ -1,19 +1,32 @@
 import { ChatRoom, ChatSettings } from '../models/index.js';
+import { sequelize } from '../db/sequelize.js';
 
 export const updateSettings = async (req, res, next) => {
-  const { name, relationshipType, allowedTopics, difficulty } = req.body;
+  const { name, relationshipType, allowedTopics } = req.body;
   const chatId = req.params.id;
+  const t = await sequelize.transaction();
 
   try {
     // Update Room Name (ChatRoom Table)
     if (name) {
-      await ChatRoom.update({ name }, { where: { id: chatId } });
+      await ChatRoom.update({ name }, { where: { id: chatId }, transaction: t });
     
+      // Update Settings Table
+        await ChatSettings.update(
+            { relationshipType, allowedTopics },
+            { where: { chatId: id }, transaction: t }
+        );
+
+        await t.commit();
 
       // Broadcast name change to all participants via Socket.io
       const io = req.app.get('io');
-      io.to(chatId).emit('room_name_updated', { chatId, newName: name });
-
+      io.to(chatId).emit('room_name_updated', { 
+        chatId, 
+        newName: name,
+        relationshipType,
+        allowedTopics
+      });
     }
 
     // Update Settings (ChatSettings Table) 
